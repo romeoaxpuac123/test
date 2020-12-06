c2ea5b3e76ba7af00ff60831992655d68d79d252
bayronaxpuac95 <48611884+romeoaxpuac123@users.noreply.github.com>
2020-12-05 19:55:01 -0600

    Create calculator_assert.js

---
+++ test/calculator_assert.js
@@ -0,0 +1,43 @@
+var assert   = require('assert');
+var calculator = require("app");
+
+describe("Calcultator tests using ASSERT module", function() {
+	before(function() {
+		// runs before all tests in this block
+		var result;
+	});
+	after(function() {
+		// runs after all tests in this block
+	});
+	beforeEach(function() {
+		// runs before each test in this block
+    });
+    afterEach(function() {
+	   // runs after each test in this block
+	   result = null;  
+	 });
+	describe("Check Sum Function: ", function() {
+			it("Check the returned value using: assert.equal(value, value): ", function() {
+				result   = calculator.sum(2);
+				assert.equal(result, 4);
+			});
+			it("Check the returned value using: assert(typeof(value, 'value')): ", function() {
+				result   = calculator.sum(2);
+				assert(typeof(result), "number");
+			});
+	});		
+	describe("Check addTested Function: ", function() {
+		it("Check the returned value using: assert.equal(value, value): ", function() {
+			result   = calculator.addTested("text");
+			assert.equal(result, "text tested");
+		});
+		it("Check the returned value using: assert(typeof(value, 'value')): ", function() {
+			result   = calculator.addTested("text");
+			assert.equal(typeof(result), "string");
+		});
+		it("Check the returned value using: assert.equal(value, value): ", function() {
+			result   = calculator.addTested("text");
+			assert.equal(result.length, 11);
+		});
+});	
+});
